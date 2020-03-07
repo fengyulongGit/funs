c30e3b549f0d27a90f91f04f0c68d31671a4881e
冯玉龙 <fengyulong@mac>
2020-03-07 19:09:40 +0800

    备课工具  1.环节流程添加分隔符  2.教学提示添加换行

--- drummer-tools/static/js/beike-template.js
+++ drummer-tools/static/js/beike-template.js
@@ -26,15 +26,16 @@ function initTemplate() {
             const data = item.data[j]
 
             htmlstr += '<p><table><tr><td class="td_width">' + (Number(j) + 1) + '</td><td>'
-            htmlstr += '<p>二级标题:&emsp;' + genText('template.data.subtitle', data.subtitle, i + '_' + j, true) + '</p>'
-            htmlstr += '<p>环节流程:&emsp;' + genText('template.data.linkFlow', data.linkFlow, i + '_' + j, true) + '</p>'
+            htmlstr += '<p>二级标题:&emsp;' + genText('template.data.subtitle', data.subtitle, i + '_' + j, 80) + '</p>'
+            htmlstr += '<p>环节流程:&emsp;' + genText('template.data.linkFlow', data.linkFlow, i + '_' + j, 70) +
+                genButton4AddSplit('template.data.linkFlow', i + '_' + j) + '</p>'
 
             // 伴奏音频
             const has_accompany = data.accompany && Object.keys(data.accompany).length > 0
             htmlstr += '<p><table><tr><td class="td_width">伴奏音频</td><td>'
             if (has_accompany) {
 
-                htmlstr += '<p>src:&emsp;' + genText('template.data.accompany.src', data.accompany.src, i + '_' + j, true) + '</p>'
+                htmlstr += '<p>src:&emsp;' + genText('template.data.accompany.src', data.accompany.src, i + '_' + j, 80) + '</p>'
                 htmlstr += '<p>duration:&emsp;' + genText('template.data.accompany.duration', data.accompany.duration, i + '_' + j) + '&emsp;秒</p>'
             }
             htmlstr += '</td><td class="td_width">' + genButton('', 'template.data.accompany', has_accompany, i + '_' + j) + '</td></tr></table></p>'
@@ -45,7 +46,7 @@ function initTemplate() {
             for (let k in data.linkTalk) {
                 const talk = data.linkTalk[k]
                 htmlstr += '<p><table><tr><td class="td_width">' + (talk.type == 0 ? '视频' : '音频') + (Number(k) + 1) + '</td><td>'
-                htmlstr += '<p>src:&emsp;' + genText('template.data.linkTalk.src', talk.src, i + '_' + j + '_' + k, true) + '</p>'
+                htmlstr += '<p>src:&emsp;' + genText('template.data.linkTalk.src', talk.src, i + '_' + j + '_' + k, 80) + '</p>'
                 if (talk.type == 1) {
                     htmlstr += '<p>时长:&emsp;' + genText('template.data.linkTalk.duration', talk.duration, i + '_' + j + '_' + k) + '&emsp;秒</p>'
                 }
@@ -60,7 +61,7 @@ function initTemplate() {
             for (let k in data.teachingTalk) {
                 const talk = data.teachingTalk[k]
                 htmlstr += '<p><table><tr><td class="td_width">' + (talk.type == 0 ? '视频' : '音频') + (Number(k) + 1) + '</td><td>'
-                htmlstr += '<p>src:&emsp;' + genText('template.data.teachingTalk.src', talk.src, i + '_' + j + '_' + k, true) + '</p>'
+                htmlstr += '<p>src:&emsp;' + genText('template.data.teachingTalk.src', talk.src, i + '_' + j + '_' + k, 80) + '</p>'
                 if (talk.type == 1) {
                     htmlstr += '<p>时长:&emsp;' + genText('template.data.teachingTalk.duration', talk.duration, i + '_' + j + '_' + k) + '&emsp;秒</p>'
                 }
@@ -69,7 +70,7 @@ function initTemplate() {
             htmlstr += '<p>' + genButton('视频', 'template.data.teachingTalk.video', false, i + '_' + j) + '&emsp;&emsp;' + genButton('音频', 'template.data.teachingTalk.audio', false, i + '_' + j) + '<p>'
             htmlstr += '</td></tr></table></p>'
 
-            htmlstr += '<p>教学提示:&emsp;' + genText('template.data.desc', data.desc, i + '_' + j, true) + '</p>'
+            htmlstr += '<p>教学提示:&emsp;<span style="color: #D12F19;">(支持回车换行)</span>' + genTextarea('template.data.desc', data.desc, i + '_' + j) + '</p>'
 
             htmlstr += '</td><td class="td_width"><p>' + genButton('', 'template.data', true, i + '_' + j) + '</p></td></tr>'
             htmlstr += '</table></p>'
@@ -87,15 +88,25 @@ function initTemplate() {
     gen()
 }
 
-function genText(mode, value, index, big) {
+function genText(mode, value, index, width) {
     index = index || 0
-    big = big || false
-    return '<input type="text" style="' + (big ? 'width:80%;' : '') + '" mode="' + mode + '" index="' + index + '" value="' + value + '" onchange="changeText(this)" />'
+    width = width || 0
+    return '<input type="text" id="text_' + (mode + '_' + index).replace(/\./g, '_') + '" style="' + (width > 0 ? 'width:' + width + '%;' : '') + '" mode="' + mode + '" index="' + index + '" value="' + value + '" onchange="changeText(this)" />'
+}
+
+function genTextarea(mode, value, index) {
+    index = index || 0
+    return '<textarea style="width: 100%;height: 100px;" mode="' + mode + '" index="' + index + '" value="' + value + '" onchange="changeText(this)" ></textarea>'
 }
 
 function genButton(flag, mode, checked, index) {
     index = index || 0
     return '<input type="button" style="width:60px;' + (checked ? 'background-color: #999999;' : '') + '" value="' + (checked ? '删除' : '添加') + flag + '" mode="' + mode + '" index="' + index + '" onclick="' + (checked ? 'delSchema(this)' : 'addSchema(this)') + '"/>'
+}
+
+function genButton4AddSplit(mode, index) {
+    index = index || 0
+    return '&emsp;<input type="button" style="width: 40px;height: 20px;" value="分隔" mode="' + mode + '" index="' + index + '" onclick="addSplit(this)"/>'
 }
 
 function delSchema(e) {
@@ -190,6 +201,23 @@ function changeText(e) {
     }
 
     gen()
+}
+
+function addSplit(e) {
+    const dom = $(e)
+    const mode = dom.attr("mode")
+    const index = dom.attr("index")
+    const split = '@drummer@'
+    if ('template.data.linkFlow' == mode) {
+        // const positions = index.split("_")
+        // template[positions[0]].data[positions[1]].linkFlow += split
+
+        const textDom = $(('#text_' + mode + '_' + index).replace(/\./g, '_'))
+        textDom.val(textDom.val() + split).trigger('change')
+        // textDom.change()
+    }
+
+    // gen()
 }
 
 function gen() {
